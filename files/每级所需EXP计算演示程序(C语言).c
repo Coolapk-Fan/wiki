d55a426e653880badb6b88cdc2973307c16b391b
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-05-21 21:31:05 +0800

    Update 每级所需EXP计算演示程序(C语言).c

--- files/每级所需EXP计算演示程序(C语言).c
+++ files/每级所需EXP计算演示程序(C语言).c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main()
+// {
+//  for( int level = 0; level < 100; level = level + 1 )
+//  printf("Hello, World! \n");
  
-int main()
-{
-   printf("Hello, World! \n");
-   
-   return 0;
-}
+//  return 0;
+// }
