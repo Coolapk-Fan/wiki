b7301e298479bd99be17146c4dd3bc3f5966f13a
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-05-21 10:05:14 -0500

    超骚的一句话算法［doge］

--- files/每级所需EXP计算演示程序(C语言).c
+++ files/每级所需EXP计算演示程序(C语言).c
@@ -1,9 +1,6 @@
-// #include <stdio.h>
-
-// int main()
-// {
-//  for( int level = 0; level < 100; level = level + 1 )
-//  printf("Hello, World! \n");
- 
-//  return 0;
-// }
+#include <stdio.h>
+int main()
+{
+for ( int exp , lv = 0 ; lv < 100 ; lv++ , exp=1000*(lv-1>0)*(lv-1)*(lv-1)*(lv-1)-((lv-1>0)-1)*lv*300 , printf("Lv%d \t 所需经验值： %d\n", lv , exp));
+return 0;
+}
