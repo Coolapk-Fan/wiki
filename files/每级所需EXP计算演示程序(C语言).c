e71e902c37167e7bc066a6d1f14d6dcc6a452211
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-05-21 10:31:02 -0500

    Update 每级所需EXP计算演示程序(C语言).c
    应该就差缩进了［doge］

--- files/每级所需EXP计算演示程序(C语言).c
+++ files/每级所需EXP计算演示程序(C语言).c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-for ( int exp , lv = 0 ; lv < 100 ; lv++ , exp=1000*(lv>1)*(lv-1)*(lv-1)*(lv-1)-(lv<2)*lv*300 , printf("Lv%d \t 所需经验值： %d\n", lv , exp));
+for ( int exp , lv = 0 ; lv < 100 ; lv++ , exp=1000*(lv>1)*(lv-1)*(lv-1)*(lv-1)+(lv<2)*lv*300 , printf("Lv%d \t 所需经验值： %d\n", lv , exp));
 return 0;
 }
