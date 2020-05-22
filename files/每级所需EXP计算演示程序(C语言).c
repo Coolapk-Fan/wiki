847b67e5a6a72bf52b7a335485fe589bf4d1fbd0
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-05-22 09:17:14 +0800

    Update 每级所需EXP计算演示程序(C语言).c

--- files/每级所需EXP计算演示程序(C语言).c
+++ files/每级所需EXP计算演示程序(C语言).c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                                                   
 int main()
 {
-    for ( int i = 0 ; i < 100 ; i++ , printf("Lv%d \t 起始经验值： %d\n", i , 1000*(i>1)*(i-1)*(i-1)*(i-1)+(i<2)*i*300 ));
+    for ( int i = 1 , start , exp ; i < 100 ; i++ )
+    {
+        start = 1000*(i>1)*(i-1)*(i-1)*(i-1)+(i<2)*i*300 ;
+        exp = start-1000*(i>2)*(i-2)*(i-2)*(i-2)-(i<3)*(i-1)*300 ;
+        printf("Lv%d \t 起始经验值： %d \t ，  从Lv%d \t 升级至 Lv%d \t 所需经验值： %d\n" , i , start , i-1 , i , exp ) ;
+    }
     return 0;
 }
