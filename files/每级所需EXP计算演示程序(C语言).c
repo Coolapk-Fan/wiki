d23b71ca3257185f134a2fca9884e2b9f0bb38ab
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-05-22 14:42:54 +0800

    Update 每级所需EXP计算演示程序(C语言).c
    使用Linux内核编码样式

--- files/每级所需EXP计算演示程序(C语言).c
+++ files/每级所需EXP计算演示程序(C语言).c
@@ -1,11 +1,19 @@
-int printf (const char *format, ...); 
+int printf (const char *format, ...);
 int main()
 {
-    for ( int i = 1 , start , exp ; i < 100 ; i++ )
-    {
-        start = 1000*(i>1)*(i-1)*(i-1)*(i-1)+(i<2)*i*300 ;
-        exp = start-1000*(i>2)*(i-2)*(i-2)*(i-2)-(i<3)*(i-1)*300 ;
-        printf("Lv%d \t 起始经验值： %d \t ，  从Lv%d \t 升级至 Lv%d \t 所需经验值： %d\n" , i , start , i-1 , i , exp ) ;
-    }
-    return 0;
+        int lv, start, exp;
+        for (lv = 1; lv < 100; lv++){
+                if (lv>1){
+                        start = 1000 * (lv-1) * (lv-1) * (lv-1);
+                }else{
+                        start = 300 * lv;
+                }
+                if (lv>2){
+                        exp = start - 1000 * (lv-2) * (lv-2) * (lv-2);
+                }else{
+                        exp = start - 300 * (lv-1);
+                }
+                printf("Lv%d \t 起始经验值： %d \t ，  从Lv%d \t 升级至 Lv%d \t 所需经验值： %d\n", lv  start, lv-1, lv, exp);
+        }
+        return 0;                                                                                                                                                    
 }
