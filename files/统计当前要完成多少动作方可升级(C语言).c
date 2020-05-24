d91f34d3e84e59132ea29ab57d5d8bb503fca8ea
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-05-24 11:15:11 +0800

    Update 统计当前要完成多少动作方可升级(C语言).c

--- files/统计当前要完成多少动作方可升级(C语言).c
+++ files/统计当前要完成多少动作方可升级(C语言).c
@@ -4,14 +4,18 @@ int main ()
         int a, b, lv, i;
         printf("输入当前经验值：\n");
         scanf("%d",&a);
-        for (i=1; i < 100; i++){
-                if (i*i*i>a/1000){
-                        lv = i;
-                        break;
+        if (a >= 1000){
+                for (i = 2; i < 100; i++){
+                        if (i*i*i>a/1000){
+                                lv = i;
+                                break;
+                        }
                 }
+        }else{
+                lv = (a>=300);
         }
         printf("您当前等级：Lv%d\n", lv);
-        b = 1000*lv*lv*lv-a;
+        b = 1000*lv*lv*lv*(lv>0)+(lv==0)*300; b-=a;
         printf("缺少 %d EXP可升下一级", b);
         //计算每日上限
         //计算赞数
